e13fc43872314a041ff550b85156647d5217eef4
Logan Arnett <logan@loganarnett.com>
2019-11-11 15:58:18 -0500

    updating helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,19 +1,23 @@
-import { Context } from 'probot';
+import { unescape } from 'lodash'
+import { Context } from 'probot'
+import stripAnsi from 'strip-ansi'
 
-import { StatusDataObject } from '../types';
+import { StatusDataObject } from '../types'
 
 const logos = {
   circleCI:
-    '<img width="400" height="125" src="http://cloud.loganarnett.com/f3467142ee70/Image%2525202019-11-08%252520at%2525202.31.28%252520AM.png">'
-};
+    '<img width="400" height="125" src="http://cloud.loganarnett.com/f3467142ee70/Image%2525202019-11-08%252520at%2525202.31.28%252520AM.png">',
+  travisCI:
+    '<img width="400" height="150" src="http://cloud.loganarnett.com/be61972f4407/Image%2525202019-11-08%252520at%2525202.31.55%252520AM.png">'
+}
 
 /**
  * Extracts the build number from the target_url
  */
 const getBuildNumber = (targetUrl: string): string => {
-  var split = targetUrl.split('/');
-  return split[split.length - 1].split('?')[0];
-};
+  var split = targetUrl.split('/')
+  return split[split.length - 1].split('?')[0]
+}
 
 /**
  * Parses the 'status' event context payload
@@ -27,7 +31,7 @@ export const getStatusData = (context: Context): StatusDataObject => {
       target_url,
       repository: { owner, name }
     }
-  } = context;
+  } = context
 
   return {
     buildNumber: getBuildNumber(target_url),
@@ -38,8 +42,8 @@ export const getStatusData = (context: Context): StatusDataObject => {
     targetUrl: target_url,
     owner: owner.login,
     repo: name
-  };
-};
+  }
+}
 
 /**
  * Requests the Pull Request number from GitHub
@@ -49,18 +53,18 @@ export const getPullRequestNumber = async (context: Context, { owner, repo, comm
     owner,
     repo,
     head: `${committerName}:${branchName}`
-  });
+  })
 
-  return allPullRequests.data[0].number;
-};
+  return allPullRequests.data[0].number
+}
 
 /**
  * Builds and Formats the comment to be applied to the Pull Request
  */
-export const buildPullRequestComment = ({ buildNumber, targetUrl, commitSha, commitUrl }: StatusDataObject): string => {
+export const buildPullRequestComment = ({ buildNumber, targetUrl, commitSha, commitUrl }: StatusDataObject, logsBuffer?: Buffer): string => {
   let pullRequestComment = `
 <p align="center">
-${logos.circleCI}
+${logsBuffer ? logos.circleCI : logos.travisCI}
 </p>
 \n
 ## Build [#${buildNumber}](${targetUrl}) Failed
@@ -68,10 +72,30 @@ ${logos.circleCI}
 This PR is moving right along but the build has failed - [#${buildNumber}](${targetUrl}).
 \n
 The commit that started this build can be found here [**${commitSha}**](${commitUrl}).
-\n
+\n`
+
+  let errorBody: string = ''
+  if (logsBuffer) {
+    // Parses the Circle CI output log buffer to unescape special characters
+    const unescapedBody = unescape(JSON.parse(logsBuffer.toString())[0].message)
+    // Strips the ANSI color codes
+    const stripColors = stripAnsi(unescapedBody)
+    // Filters npm info and err lines out for a more focused error
+    errorBody = stripColors
+      .split('\n')
+      .filter((line: string) => !line.includes('npm info') && !line.includes('npm ERR'))
+      .join('')
+
+    pullRequestComment += `
+\`\`\`bash
+${errorBody}
+\`\`\``
+  }
+
+  pullRequestComment += `
 Once you have updated the PR and the build is passing then :shipit:
 \n
-`;
+`
 
-  return pullRequestComment;
-};
+  return pullRequestComment
+}
